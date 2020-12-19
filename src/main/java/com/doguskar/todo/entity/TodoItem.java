f61dbcaaa7784798351022048516a78e572c1070
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2020-12-19 15:39:33 +0300

    db problem fixed

--- src/main/java/com/doguskar/todo/entity/TodoItem.java
+++ src/main/java/com/doguskar/todo/entity/TodoItem.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "todo_item")//, schema = "todo"
 public class TodoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/doguskar/todo/entity/User.java
+++ src/main/java/com/doguskar/todo/entity/User.java
@@ -9,6 +9,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Table(name = "app_user")//, schema = "todo"
 public class User implements UserDetails{
     @Id
     @GeneratedValue
